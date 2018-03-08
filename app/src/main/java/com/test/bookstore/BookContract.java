1d8bd0bacf3bb557d66d7ed1c0e1ffded966d98e
PyrovSergey <pyrov.sergey@gmail.com>
2018-03-08 16:00:08 +0300

    update_1

---
+++ app/src/main/java/com/test/bookstore/BookContract.java
@@ -0,0 +1,28 @@
+package com.test.bookstore;
+
+
+import android.provider.BaseColumns;
+
+public final class BookContract {
+
+    private BookContract() {
+    }
+
+    public static final class BookEntry implements BaseColumns {
+
+        public static final String TABLE_NAME = "books";
+
+        public static final String _ID = BaseColumns._ID;
+
+        public static final String COLUMN_BOOK_NAME = "product name";
+
+        public static final String COLUMN_BOOK_PRICE = "price";
+
+        public static final String COLUMN_BOOK_QUANTITY = "quantity";
+
+        public static final String COLUMN_BOOK_SUPPLIER_NAME = "supplier name";
+
+        public static final String COLUMN_BOOK_SUPPLIER_PHONE_NUMBER = "supplier phone number";
+    }
+
+}
